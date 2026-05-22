00819e587dd4747e5f86fa8f106a59439e2586c1
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2026-05-22 16:22:27 -0300

    revenda

---
+++ Programas/revenda.js
@@ -0,0 +1,24 @@
+// Elaborar um programa para uma revenda de veículos. O programa deve ler modelo e preço do veículo. 
+// Apresentar como resposta o valor da entrada (50%) e o saldo em 12x.
+// Crie o arquivo revenda.html e revenda.js 
+
+
+// adiciona pacotes node ao programa
+const prompt = require("prompt-sync")();
+
+// le o nome do veiculo
+const veiculo = prompt("Digite o modelo do veiculo:");
+
+// le o preço do veiculo
+const preco = Number(prompt("Digite o Preço:"));
+
+// calcula o valor da entrada
+const entrada = preco * 0.5;
+
+// calcula o valor da parcela
+const parcela = (preco * 0.5) / 12;
+
+// exibe o nome do veiculo e demais saidas
+console.log(`Promoção:${veiculo}`);
+console.log(`Entrada de: R$ ${entrada.toFixed(2)}`);
+console.log(`+ 12x de: R$ ${parcela.toFixed(2)}`);
